6a31fa27f51e8bfb3ed60577743910712ca85a0a
Yiem <642092830@qq.com>
2018-03-29 15:25:06 +0800

    初始

---
+++ iOS_Tools/iOS_Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Tools
+//
+//  Created by em_er on 2018/3/29.
+//  Copyright © 2018年 YIem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Tools/iOS_Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Tools
+//
+//  Created by em_er on 2018/3/29.
+//  Copyright © 2018年 YIem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
